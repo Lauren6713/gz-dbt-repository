46142c6b742e7a6b6d58829a8c299597cc0ad83d
lauren.garcia0204@gmail.com <lauren.garcia0204@gmail.com>
2025-02-18 15:05:48 +0000

    Margin per order

--- models/int_orders_margin.sql
+++ models/int_orders_margin.sql
@@ -1,5 +1,8 @@
-select*
-
-
+select
+date_date,
+ROUND(SUM (revenue)) AS total_revenue,
+SUM(quantity) AS total_quantity,
+ROUND(SUM(margin)) AS total_margin
 from {{ ref('int_sales_margin') }}
 GROUP BY date_date
+
