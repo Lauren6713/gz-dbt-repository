76cc3eda897b7ae4915d0477822e7413b2a2504b
lauren.garcia0204@gmail.com <lauren.garcia0204@gmail.com>
2025-02-18 15:35:07 +0000

    adding shipping fees

--- models/int_orders_margin.sql
+++ models/int_orders_margin.sql
@@ -1,8 +1,24 @@
-select
-date_date,
-ROUND(SUM (revenue)) AS total_revenue,
-SUM(quantity) AS total_quantity,
-ROUND(SUM(margin)) AS total_margin
-from {{ ref('int_sales_margin') }}
-GROUP BY date_date
-
+WITH orders_margin AS (
+    SELECT *
+    FROM {{ ref('int_sales_margin') }}
+),
+shipping AS (
+    SELECT *
+    FROM {{ ref('stg_raw__ship') }}
+)
+SELECT
+    orders_margin.orders_id,
+    orders_margin.date_date,
+    orders_margin.margin,
+    shipping.shipping_fee,
+    CAST(shipping.ship_cost AS FLOAT64) AS ship_cost,
+    CAST(shipping.logcost AS FLOAT64) AS logcost,
+    CAST(
+        orders_margin.margin + shipping.shipping_fee - shipping.logcost - shipping.ship_cost
+        AS FLOAT64
+    ) AS operational_margin
+FROM
+    orders_margin
+LEFT JOIN
+    shipping
+ON   orders_margin.orders_id = shipping.orders_id
